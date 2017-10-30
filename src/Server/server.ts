4b51a4da4c0108ad03172d346a56789972fc5092
whisper <whisper.fairy@163.com>
2017-10-30 19:43:15 +0800

    init

--- src/Server/server.ts
+++
@@ -1,7 +0,0 @@
-import {bar} from './dao/index';
-export function foo() {
-    console.log('foo');
-    bar();
-    console.log('执行完毕');
-}
-foo();
---
+++ test.js
@@ -0,0 +1,10 @@
+/**
+ *
+ * 文件作用
+ * @author Livermzsh <Livermzsh@hotmail.com>
+ * @date 2017/10/24
+ *
+ * @内容 作用
+ * @内容 作用
+ */
+
